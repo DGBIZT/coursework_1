eae55b3dc612afcbd2ea37779125d1391affdcce
George Gorlov <Georgiy.Gorlov@gmail.com>
2025-04-21 16:34:30 +0300

    Добавил файл xlsx

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+import datetime
+a = "hello world"
+date_obj = datetime.datetime.now()
+print(a, date_obj.strftime("%H:%M.%S"))
+
+time_only = date_obj.time().replace(microsecond=0)
+print(time_only)
