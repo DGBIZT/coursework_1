4ca10bcef1e1bdff5536ec0bfb477074c9005f46
DGBIZT <georgiy.gorlov@gmail.com>
2025-04-26 17:14:48 +0300

    Добавил декоратор по отчету

--- src/reports.py
+++ src/reports.py
@@ -4,6 +4,32 @@ import datetime
 from datetime import datetime
 from typing import Optional
 import os
+from functools import wraps
+
+def report_to_file(func):
+    """Декоратор для записи результатов отчета в файл"""
+
+    @wraps(func)
+    def inner(*args, **kwargs):
+
+        result = func(*args, **kwargs)
+
+        now = datetime.now().strftime("%d-%m-%Y_%H-%M")
+        # Создаём абсолютный путь
+        base_dir = os.path.dirname(os.path.abspath(__file__))  # Получаем директорию текущего файла
+        reports_dir = os.path.join(base_dir, '../reports')  # Создаём папку reports
+
+        # Создаём папку, если её нет
+        os.makedirs(reports_dir, exist_ok=True)
+
+        file_path = os.path.join(reports_dir, f"{now}_report.json") ## Формируем полный путь к файлу
+
+        with open(file_path, 'w', encoding='utf-8') as f:
+            json.dump(result, f, ensure_ascii=False, indent=4)
+
+        return result
+    return inner
+
 
 
 
@@ -17,6 +43,7 @@ def read_xlsx_file(file_path: str) -> pd.DataFrame:
 
     return excel_data
 
+@report_to_file
 def spending_by_category(transactions: pd.DataFrame,
                          category: str,
                          date: Optional[str] = None) -> pd.DataFrame:
@@ -31,9 +58,9 @@ def spending_by_category(transactions: pd.DataFrame,
         start_date = end_date - pd.DateOffset(months=3)
     else:
         now = datetime.now()
-        # Создал переменную hour, куда получил значение текущего времени в часах now.hour
-        hour = now
-        end_date = pd.to_datetime(hour, format='%d.%m.%Y')
+        # Создал переменную data_now, куда получил значение текущей даты
+        data_now = now
+        end_date = pd.to_datetime(data_now, format='%d.%m.%Y')
         # Начальная дата, отнимаем от конечной даты 3 месяца
         start_date = end_date - pd.DateOffset(months=3)
 
@@ -52,4 +79,4 @@ def spending_by_category(transactions: pd.DataFrame,
 data_frame_new = read_xlsx_file("../data/operations.xlsx")
 
 report_category = spending_by_category(data_frame_new, "ЖКХ", "16.12.2021")
-print(report_category)
+# print(report_category)
