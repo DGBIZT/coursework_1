0883e65d09df0d05435e74f2c68c018c25ed84d6
DGBIZT <georgiy.gorlov@gmail.com>
2025-04-26 13:18:52 +0300

    Добавил функция простого поиска в модуль services

--- src/services.py
+++ src/services.py
@@ -1,35 +1,31 @@
-import os
 import pandas as pd
-import datetime
-import json
 import os
-# from collections import defaultdict
-from datetime import datetime
+
 
 
 import requests
 from dotenv import load_dotenv
 
-def simple_search(file_path: str, ): #, search: str
+def simple_search(file_path: str ): #, search: str
 
     bas_dir = os.path.dirname(__file__)
     full_path = os.path.join(bas_dir, file_path)
 
     excel_data = pd.read_excel(full_path)
 
-    print("Осуществляется по категории или по описанию")
-    search_enter = ("Введите запрос \n")
+    print("Поиск осуществляется по категории или по описанию")
+    search_enter = input("Введите запрос \n")
 
     if search_enter:
-        # sum_dict = excel_data.groupby("Дата платежа")["Статус"].sum().to_dict()
-        sim_sea = excel_data[excel_data['Категория'] == search_enter]
+        # Осуществляем поиск по двум столбцам
+        sim_sea = excel_data[(excel_data["Описание"] == search_enter) | (excel_data["Категория"] == search_enter)]
+        # Затем проверяем свойство .empty у DataFrame - оно возвращает True, если DataFrame пустой
+        if sim_sea.empty:
+            return "По данному запросу отсутствуют транзакции"
+        else:
+            json_data = sim_sea.to_json(orient='records', force_ascii=False, indent=4)
+            return json_data
 
 
-
-
-
-        return sim_sea
-
-
-list_dicts = simple_search("../data/operations.xlsx") #", Супермаркеты"
-print(list_dicts)
+file_path = simple_search("../data/operations.xlsx")
+print(file_path)
--- src/views.py
+++ src/views.py
@@ -1,6 +1,6 @@
 # import datetime
 # import os
-# import pandas as pd
+import pandas as pd
 # from collections import defaultdict
 # from datetime import datetime
 import json
@@ -8,17 +8,26 @@ import json
 from utils import cards, currency_rate, hello_client, read_transactions_excel_and_output, top_transactions, user_stocks
 
 
+def enter_input() -> pd.DataFrame:
+    """Функция для ввода пользователем даты и время"""
+    date_enter = input("Введите дату в формате d.m.Y H:M:S \n")
+    struct_file_json = read_transactions_excel_and_output(
+        "../data/operations.xlsx", date_enter
+    )
+    return struct_file_json
+
 def page_main():
     """Функция главной страницы возвращает основную информацию"""
     # period_date = get_period_date(date)
-    date_enter = input("Введите дату в формате d.m.Y H:M:S \n"  )
-    struct_file_json = read_transactions_excel_and_output(
-        "../data/operations.xlsx", date_enter
-    )  # read_finance_exel_operastion
+    # date_enter = input("Введите дату в формате d.m.Y H:M:S \n"  )
+    # struct_file_json = read_transactions_excel_and_output(
+    #     "../data/operations.xlsx", date_enter
+    # )  # read_finance_exel_operastion
+    tabl_new = enter_input()
     json_response = {
         "greeting": hello_client(),
-        "cards": cards(struct_file_json),  # main_cards(struct_file_json)
-        "top_transactions": top_transactions(struct_file_json),
+        "cards": cards(tabl_new),  # main_cards(struct_file_json)
+        "top_transactions": top_transactions(tabl_new),
         # "currency_rates": currency_rate("../data/user_settings.json"),
         # "stock_prices": user_stocks("../data/user_settings.json"),
     }
