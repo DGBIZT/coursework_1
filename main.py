9f44a9f2207f11fb8064d4fbf3c2a775d618fb1a
DGBIZT <georgiy.gorlov@gmail.com>
2025-04-30 21:57:03 +0300

    main

--- main.py
+++ main.py
@@ -39,21 +39,3 @@ def basic_logic_2():
 
 
 basic_logic_2()
-# def basic_logic():
-#     """Функция отвечающая за основную логику проекта и связывает функциональности между собой"""
-#
-#     print("Данные для анализа и вывода на веб-страницах.")
-#     start_basic_logic = page_main()
-#     print(start_basic_logic)
-#
-#     print("Так же вы можете осуществить поиск по всем транзакциям.")
-#     start_simple_search = simple_search("../data/operations.xlsx")
-#     print(start_simple_search)
-#
-#     print("Получите отчет за последние три месяца по категориям")
-#     data_frame_new = read_xlsx_file("../data/operations.xlsx")
-#     you_сategory = input("Введити категорию по которой вы хотите получить отчет. \n")
-#     print("Если дата не будет введена, то берется текущая дата")
-#     you_date = input("Введите дату \n")
-#     you_report = spending_by_category(data_frame_new, you_сategory, you_date)
-#     print(you_report)
